829e76e23246bde6b1cc26f372a5ef936be79b18
sam <s.ojling@gmail.com>
2021-04-23 15:53:03 -0700

    Fixing login issue

--- utils/server.ts
+++ utils/server.ts
@@ -5,8 +5,10 @@ export const makeServer = () => {
     routes() {
       this.post("/login", (_, request) => {
         let user = JSON.parse(request.requestBody);
-
-        if (user.email !== "sam@ojling.com" && user.password !== "Password1") {
+        console.log(user);
+        if (
+          !(user.email === "sam@ojling.com" && user.password === "Password1")
+        ) {
           return new Response(
             403,
             {},
